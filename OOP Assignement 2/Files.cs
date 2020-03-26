9f78f1b1ae9682788475c6b45e17b850455e0599
GrimIron <60586368+GrimIron@users.noreply.github.com>
2020-03-26 17:40:17 +0000

    done

---
+++ OOP Assignement 2/Files.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace OOP_Assignement_2
+{
+    public class Files
+    {
+        string a_txt { get; set; }
+        string b_txt { get; set; }
+
+        public bool fileCheck(string file_1, string file_2)
+        {
+            a_txt = getLocation(file_1);            //gets the location for the first file          
+            b_txt = getLocation(file_2);            //gets the location for the second file
+            try
+            {
+                FileStream txt1 = new FileStream(a_txt, FileMode.Open, FileAccess.Read);
+                FileStream txt2 = new FileStream(b_txt, FileMode.Open, FileAccess.Read);
+
+                //checks if the files are different
+                if (txt1.Length != txt2.Length)
+                {
+                    //closes the files-
+                    txt1.Close();
+                    txt2.Close();
+
+                    string not_same = file_1 + " and " + file_2 + " are different";
+                    Console.WriteLine(not_same);
+                    return true;
+                }
+                else
+                {
+                    //closes the files
+                    txt1.Close();
+                    txt2.Close();
+
+                    string same = file_1 + " and " + file_2 + " are not different";
+                    Console.WriteLine(same);
+                    return true;
+                }
+            }
+            //handles if the file is not found
+            catch (FileNotFoundException)
+            {
+                string exception = "one or both of the files do not exist!";
+                Console.WriteLine(exception);
+                return false;
+            }
+        }
+
+        //gets the location of the file the user inputted
+        private string getLocation(string file_name)
+        {
+            string current_dir = Directory.GetCurrentDirectory();
+            string newPath = Path.GetFullPath(Path.Combine(current_dir, @"..\..\..\" + file_name));
+
+            return newPath;
+        }
+    }
+}
---
+++ OOP Assignement 2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+namespace OOP_Assignement_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool check = false;
+            //loops until check = true
+            do
+            {
+                Files f = new Files();
+                string[] input = user_input();
+
+                //checks which command the user has chosen to use
+                if (input[0] == "diff")
+                {
+                    check = f.fileCheck(input[1], input[2]);
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Command!");
+                }
+            }
+            while (check != true);
+            Console.ReadLine();
+        }
+
+        //gets the user input formats it and error checks it
+        static string[] user_input()
+        {
+            bool check = false;
+            string[] command;
+            //loops untill check = true
+            do
+            {
+                Console.Write("> ");                                                                    //shows that the users needs to input
+                string user_input = Console.ReadLine().ToLower();                                       //asks the user to input a string 
+                command = user_input.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);   //splits the string on a space places into a string array, removes any white space
+                int total = 0;                                                                          //counter to keep track of which element
+
+                //checks to see if too many arguments have been included
+                foreach (string a in command)
+                {
+                    total++;
+                    if (total > 3)
+                    {
+                        Console.WriteLine("Too many arguments! should be: command file file");
+                    }
+                }
+
+                //checks to see if no command was entered, and if too little arguments were given 
+                if (command == null || command.Length == 0)
+                {
+                    Console.WriteLine("There is no command!");
+                }
+                else if (total < 3)
+                {
+                    Console.WriteLine("Too little arguments! should be: command file file");
+                }
+                else
+                {
+                    check = true;
+                }
+            }
+            while (check != true);
+            
+            //returns the command and files chosen by the user
+            return command;
+        }
+    }
+}
